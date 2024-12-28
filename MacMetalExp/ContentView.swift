70fd158528518ed78c15fcfb85fd38d437a19965
Carlos Mbendera <carlosmbendera@gmail.com>
2024-12-28 21:15:58 +0000

    Initial Commit

---
+++ MacMetalExp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MacMetalExp
+//
+//  Created by Carlos Mbendera on 28/12/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MacMetalExp/MacMetalExpApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacMetalExpApp.swift
+//  MacMetalExp
+//
+//  Created by Carlos Mbendera on 28/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct MacMetalExpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
